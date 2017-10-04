f7ec6d815640d8b5839d2816ed6ed76a1fdf042b
Kate Khoreshko <khoreshko.ev@gmail.com>
2017-10-04 05:25:11 +0300

    Update code.js

--- HW1/code.js
+++ HW1/code.js
@@ -1,11 +1,12 @@
 var arr = [];
+	arr[2] = 'All successful men and women are big dreamers. They imagine what their future could be, ideal in every respect, and then they work every day toward their distant vision, that goal or purpose.” – Brian Tracy';
+	arr[1] = 'Genius is one percent inspiration and ninety-nine percent perspiration. – Thomas Edison';
+	arr[0] = 'You can observe a lot just by watching. – Yogi Berra';
 var pos;
 
 function day_quote()
 {
-	arr[2] = 'All successful men and women are big dreamers. They imagine what their future could be, ideal in every respect, and then they work every day toward their distant vision, that goal or purpose.” – Brian Tracy';
-	arr[1] = 'Genius is one percent inspiration and ninety-nine percent perspiration. – Thomas Edison';
-	arr[0] = 'You can observe a lot just by watching. – Yogi Berra';
+
 	pos = Math.round(Math.random()*(arr.length-1));
 	document.getElementById('main_q').innerHTML = arr[pos];
 
