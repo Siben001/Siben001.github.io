8418743da8fce95c8ed2961b5c1c8a712ddde33d
Kate Khoreshko <khoreshko.ev@gmail.com>
2017-10-04 03:48:56 +0300

    Update code.js

--- HW1/code.js
+++ HW1/code.js
@@ -10,7 +10,7 @@ function day_quote()
 	
 	pos = Math.round(Math.random()*(arr.length-1));
 	document.getElementById('info').innerHTML = arr[pos];
-	arr1 = arr;
+	arr1 = arr.slice(0);
 	arr1.splice(pos,1);
 	allert(arr1);
 }
